dbb57082f0547cd69b17f7bb8a7cea9829e35a0d
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-10-26 04:15:28 +0900

    Add VideoUploadPage UI

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 import Dropzone from "react-dropzone";
@@ -6,17 +6,49 @@ import Dropzone from "react-dropzone";
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+    { value: 0, label: "Private" },
+    { value: 1, label: "Public" },
+];
+const CategoryOptions = [
+    { value: 0, label: "Film & Animation" },
+    { value: 1, label: "Autos & Venhicles" },
+    { value: 2, label: "Music" },
+    { value: 3, label: "Pets & Animals" },
+];
+
 function VideoUploadPage() {
+    const [VideoTitle, setVideoTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [Private, setPrivate] = useState(0);
+    const [Category, setCategory] = useState("Film & Animation");
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value);
+    };
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value);
+    };
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value);
+    };
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value);
+    };
+
     return (
         <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
             <div style={{ textAlign: "center", marginBottom: "2rem" }}>
                 <Title level={2}>Upload Video</Title>
             </div>
 
-            <Form onSubmit>
+            <Form>
                 <div style={{ display: "flex", justifyContent: "space-between" }}>
                     {/* Drop Zone */}
-                    <Dropzone onDrop multiple maxSize>
+                    <Dropzone>
                         {({ getRootProps, getInputProps }) => (
                             <div
                                 style={{
@@ -42,27 +74,40 @@ function VideoUploadPage() {
                 <br />
                 <br />
                 <label>Title</label>
-                <Input onChange value />
+                <Input value={VideoTitle} onChange={onTitleChange} />
 
                 <br />
                 <br />
                 <label>Description</label>
-                <TextArea onChange value />
+                <TextArea value={Description} onChange={onDescriptionChange} />
 
                 <br />
                 <br />
-                <select onChange>
-                    <option key={true} value></option>
-                </select>
-                <br />
-                <br />
-                <select onChange>
-                    <option key={true} value></option>
+                <select onChange={onPrivateChange}>
+                    {PrivateOptions.map((item, index) => {
+                        return (
+                            <option key={index} value={item.value}>
+                                {item.label}
+                            </option>
+                        );
+                    })}
                 </select>
 
                 <br />
                 <br />
-                <Button type="primary" size="large" onClick>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => {
+                        return (
+                            <option key={index} value={item.value}>
+                                {item.label}
+                            </option>
+                        );
+                    })}
+                </select>
+
+                <br />
+                <br />
+                <Button type="primary" size="large">
                     Submit
                 </Button>
             </Form>
