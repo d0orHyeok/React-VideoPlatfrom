7a4209f1b097d5c1f4bd72c68f86e4a3de451a1c
JangHyeok Kim <48301140+d0orHyeok@users.noreply.github.com>
2021-10-26 21:09:22 +0900

    썸네일 생성 구현

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -23,6 +23,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("");
     const [Private, setPrivate] = useState(0);
     const [Category, setCategory] = useState("Film & Animation");
+    const [FilePath, setFilePath] = useState("");
+    const [Duration, setDuration] = useState("");
+    const [ThumbnailPath, setThumbnailPath] = useState("");
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value);
@@ -49,7 +52,21 @@ function VideoUploadPage() {
 
         Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
             if (response.data.success) {
-                console.log(response.data);
+                let variable = {
+                    url: response.data.url,
+                    filename: response.data.filename,
+                };
+
+                setFilePath(response.data.url);
+
+                Axios.post("/api/video/thumbnail", variable).then((response) => {
+                    if (response.data.success) {
+                        setDuration(response.data.fileDuration);
+                        setThumbnailPath(response.data.url);
+                    } else {
+                        alert("Create Thumbnail Fail!");
+                    }
+                });
             } else {
                 alert("Video Upload Fail!");
             }
@@ -84,9 +101,11 @@ function VideoUploadPage() {
                         )}
                     </Dropzone>
                     {/* Thumbnail */}
-                    <div>
-                        <img src="" alt="" />
-                    </div>
+                    {ThumbnailPath && (
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
+                        </div>
+                    )}
                 </div>
                 <br />
                 <br />
